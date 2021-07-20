db9793d5172579ddc2a1fae5c60c4bfb6c0857d0
HESHAM47GAMAL <77234053+HESHAM47GAMAL@users.noreply.github.com>
2021-07-20 19:47:06 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,53 +2,52 @@
 
 int main()
 {
-    double a, b, c, d, e, f, g, h; // initialization eight varible
+    double num1, num2, num3, num4, num5, num6, num7,num8; // initialization eight varible
     printf("please enter number one: ") ;//asking user enter number one
-scanf("%lf", &a) ;
+scanf("%lf", &num1) ;
 printf("please enter number two: ") ;//asking user enter number two
-scanf("%lf", &b) ;
+scanf("%lf", &num2) ;
 printf("please enter number three: ") ;//asking user enter number three
-scanf("%lf", &c) ;
+scanf("%lf", &num3) ;
 printf("please enter number four: ") ;//asking user enter number four
-scanf("%lf", &d) ;
+scanf("%lf", &num4) ;
 printf("please enter number five: ") ;//asking user enter number five
-scanf("%lf", &e) ;
+scanf("%lf", &num5) ;
 printf("please enter number six: ") ;//asking user enter number six
-scanf("%lf", &f) ;
+scanf("%lf", &num6) ;
 printf("please enter number seven: ") ;//asking user enter number seven
-scanf("%lf", &g) ;
+scanf("%lf", &num7) ;
 printf("please enter number eight: ") ;//asking user enter number eight
-scanf("%lf", &h) ;
-double max =a;// assume first number is max number
-if(max<b)//<-------1   ///in every if condition check if any number bigger than current max and if bigger this number become current max and continue until if condition end
+scanf("%lf", &num8) ;
+double max =num1;// assume first number is max number
+if(max<num2)//<-------1   ///in every if condition check if any number bigger than current max and if bigger this number become current max and continue until if condition end
 {
-max=b;
+max=num2;
 }
-if(max<c)//<-------2
+if(max<num3)//<-------2
 {
-max=c;
+max=num3;
 }
-if(max<d)//<-------3
+if(max<num4)//<-------3
 {
-max=d;
+max=num4;
 }
-if(max<e)//<-------4
+if(max<num5)//<-------4
 {
-max=e;
+max=num5;
 }
-if(max<f)//<-------5
+if(max<num6)//<-------5
 {
-max=f;
+max=num6;
 }
-if(max<g)//<-------6
+if(max<num7)//<-------6
 {
-max=g;
+max=num7;
 }
-if(max<h)//<-------7
+if(max<num8)//<-------7
 {
-max=h;
+max=num8;
 }
 printf("max value between eight number: %lf", max) ;
     return 0;
 }
-
